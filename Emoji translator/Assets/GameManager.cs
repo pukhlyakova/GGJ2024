da4500679a42a63d7caf5504c154372fa5602e6b
Maria Del <del.maria.1991@gmail.com>
2024-01-28 13:24:18 +0100

    Add compare result logic

--- Emoji translator/Assets/GameManager.cs
+++ Emoji translator/Assets/GameManager.cs
@@ -16,12 +16,8 @@ public class Quote {
         this.allOptions.AddRange(correctAnswer);
         this.allOptions.AddRange(otherOptions);
         this.allOptions.Sort();
+        this.correctAnswer.Sort();
     }
-}
-
-public class ButtonState {
-    public bool isClicked;
-    public string spritePath;
 }
 
 public class GameManager : MonoBehaviour
@@ -32,11 +28,19 @@ public class GameManager : MonoBehaviour
     public TMP_Text score;
     public TMP_Text emoji;
 
+    public Sprite defualtButtonImg;
+
+    public Sprite selectedButtonImg;
+
     private int correctAnswers = 0;
     private int seenQuestions = 0;
 
     private int index = 0;
     private List<Quote> quoteList;
+
+    public List<Button> buttons;
+
+    public List<bool> buttonsState;
 
     static Quote quote1 = new Quote("Global Game Jam", new List<string>{"Global", "Game", "Jam"}, new List<string>{"Planet", "Gamepad", "Candy"});
     static Quote quote2 = new Quote("Eat, Sleep, Code, Repeat", new List<string>{"Eat", "Sleep", "Code", "Repeat"}, new List<string>{"Berger", "Zzzzz", "Computer", "Arrows"});
@@ -46,31 +50,65 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         quoteList = new List<Quote>{quote1, quote2, quote3};
-        updateScene();
+        UpdateScene();
     }
 
     public void TranslateClicked() {
-        correctAnswers++;
+        CheckTheAnswer();
         index = (index + 1) % quoteList.Count;
-        updateScene();
+        UpdateScene();
     }
 
-    public void selectWord() {
-        // do nothing
+    public void CheckTheAnswer() {
+        // Get the answer
+        List<string> correctAnswer = new List<string>();
+        for (int i = 0; i < buttonsState.Count; ++i) {
+            if (buttonsState[i]) {
+                correctAnswer.Add(buttons[i].GetComponentInChildren<TextMeshProUGUI>().text);
+            }
+        }
+        correctAnswer.Sort();
+        // Compare the Answer
+        if (correctAnswer.Count != quoteList[index].correctAnswer.Count) {
+            return;
+        }
+
+        for (int i = 0; i < correctAnswer.Count; ++i) {
+            if (correctAnswer[i] != quoteList[index].correctAnswer[i]) {
+                return;
+            }
+        }
+
+        correctAnswers++;
     }
 
-    private void updateScene() {
+    public void SelectWord(int index) {
+        if (buttonsState[index]) {
+            buttonsState[index] = false;
+            buttons[index].image.sprite = defualtButtonImg;
+        } else {
+            buttonsState[index] = true;
+            buttons[index].image.sprite = selectedButtonImg;
+        }
+    }
+
+    private void UpdateScene() {
         // Clear panel
+        buttons.Clear();
+        buttonsState.Clear();
         for (var i = panelToAttachButtonsTo.transform.childCount - 1; i >= 0; i--)
         {
             Object.Destroy(panelToAttachButtonsTo.transform.GetChild(i).gameObject);
         }
         // Add new buttons
         for (int i = 0; i < quoteList[index].allOptions.Count; ++i) {
+            var i1 = i;
             Button button = (Button)Instantiate(buttonPrefab);
             button.transform.SetParent(panelToAttachButtonsTo.transform);
-            button.GetComponent<Button>().onClick.AddListener(selectWord);
+            button.GetComponent<Button>().onClick.AddListener(() => SelectWord(i1));
             button.GetComponentInChildren<TextMeshProUGUI>().text = quoteList[index].allOptions[i];
+            buttons.Add(button);
+            buttonsState.Add(false);
         }
         emoji.text = quoteList[index].emoji;
         score.text = "Score: " + correctAnswers + "/" + seenQuestions;
